eee6b7aa56f3a06384c308bbbd5cb75bb7b587b9
Gaurav <gsharma.jai@salucro.com>
2021-12-15 21:15:50 +0530

    Fixed import related errors

---
+++ gophercises/Adventure/cmd/AdventureWeb/main.go
@@ -0,0 +1,11 @@
+// package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	adventure "golang_practice/gophercises/Adventure"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"strings"
+// )
---
+++ gophercises/Adventure/story.go
@@ -0,0 +1,13 @@
+// package adventure
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"html/template"
+// 	"io"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"strings"
+// )
+
