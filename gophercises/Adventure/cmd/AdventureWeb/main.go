40a32fa3f1ee0c14de83fb43730237d13b8d5d95
Gaurav <gsharma.jai@salucro.com>
2021-12-16 10:50:36 +0530

    Updated Adventures

--- gophercises/Adventure/cmd/AdventureWeb/main.go
+++ gophercises/Adventure/cmd/AdventureWeb/main.go
@@ -1,11 +1,37 @@
-// package main
+package main
 
-// import (
-// 	"flag"
-// 	"fmt"
-// 	adventure "golang_practice/gophercises/Adventure"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"strings"
-// )
+import (
+	"flag"
+	"fmt"
+	adventure "golang_practice/gophercises/Adventure"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 5500, "The Port to start Choose Your own adventure")
+	filename := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
+	flag.Parse()
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+	story, err := adventure.JsonStory(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// d := json.NewDecoder(f)
+	// var story adventure.Story
+	// if err := d.Decode(&story); err != nil {
+	// 	panic(err)
+	// }
+
+	h := adventure.NewHandler(story)
+
+	fmt.Printf("starting server at %d\n", *port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
--- gophercises/Adventure/story.go
+++ gophercises/Adventure/story.go
@@ -1,13 +1,72 @@
-// package adventure
+package adventure
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"html/template"
-// 	"io"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"strings"
-// )
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
 
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Choose your own adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+type Story map[string]Chapter
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
